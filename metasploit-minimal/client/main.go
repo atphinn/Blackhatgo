77885ca5c1ea073ded43f24a41e00ad08d6864e0
th3g3ntl3m4n <th3g3ntl3m4n@localhost.localdomain>
2020-12-22 19:36:44 -0500

    crated metaspliot api

---
+++ metasploit-minimal/client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"$GOPATH/BlackHatGo/metasploit-minimal/rpc"
+)
+
+func main()  {
+	host := os.Getenv("MSFHOST")
+	pass := os.Getenv("MSFPASS")
+	user := "msf"
+
+	if host == "" || pass == "" {
+		log.Fatalln("Missing required enviorment varible MSFHOST or MSFPASS")
+	}
+	msf, err := rpc.New(host, user, pass)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer msf.Logout()
+
+	sessions, err := msf.SessionList()
+	if err != nil {
+		log.Panicln(err)
+	}
+	fmt.Println("Sessions:")
+
+	for _, session := range sessions {
+		fmt.Printf("%e5d %s\n", session.ID, session.Info)
+	}
+		
+	
+}
